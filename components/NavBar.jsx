eff35216ba29534ad05f3780552a10dfe5a271aa
Prshnt Bhushal <prshnt.bhushal@gmail.com>
2023-05-19 23:44:00 +0545

    basic changes

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -16,7 +16,7 @@ const navbar = () => {
       <div className='flex justify-between items-center w-full h-full px-2 xl:px-10'>
        <div className=' flex items-center'>
         <Link href='/'>
-          <Image src='/../public/Images/schoollogo.png' alt='Main Logo' width='65' height='60'/>
+          <Image src='/../public/Images/waveFlag.gif' alt='Main Logo' width='65' height='60'/>
         </Link>
         <div className='mx-3 justify-center cursor-default'>
           <h3>दैलेखी सेवा समाज</h3>
@@ -88,31 +88,8 @@ const navbar = () => {
                     </ul>
                 </div>
             </div>
-
         </div>
     </div>
-    
-    // <div className='flex mx-2 justify-between w-screen'>
-    //   <div className='flex'>
-    //     <div>
-    //     <Link href='/#home'>
-    //             <Image src='/../public/Images/schoollogo.png' alt='school logo' width='75' height='25'/>
-    //         </Link>
-    //   </div>
-    //   <div className='py-4 px-2 cursor-pointer'>
-    //     <h3>दैलेखी सेवा समाज</h3>
-    //     <span>काठमाडौँ, नेपाल</span>
-    //   </div>
-    //   </div>
-    //   <div className='m-1  py-4 px-2'>
-    //     <ul className='flex justify-between'>
-    //       <li className='mx-1 text-lg hover:bg-[#b5b3b3]'><Link href='/home'>Home</Link></li>
-    //       <li className='mx-1 text-lg hover:bg-[#b5b3b3]'><Link href='/home'>About US</Link></li>
-    //       <li className='mx-1 text-lg hover:bg-[#b5b3b3]'><Link href='/home'>Projects</Link></li>
-    //       <li className='mx-1 text-lg hover:bg-[#b5b3b3]'><Link href='/home'>Contact US</Link></li>
-    //     </ul>
-    //   </div>
-    // </div>
 
   )
 }

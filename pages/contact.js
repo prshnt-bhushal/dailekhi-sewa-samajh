d7f64570ff17eca6ab1dbf62caa2e36f3b9ce2be
susanhsrestha <susanshrestha783@gmail.com>
2023-05-29 13:49:49 +0545

    added navbar styles

--- pages/contact.js
+++ pages/contact.js
@@ -2,29 +2,28 @@ import React from 'react'
 import MapComponent from '@/components/MapComponent'
 import FeedBackBox from '@/components/FeedBackBox'
 
-const  contact = () => {
+const contact = () => {
   return (
     <div className='pt-[80px] mx-10 h-min mb-14 '>
-      <h1 className='uppercase text-xl'>contact</h1>
       {/* form and message */}
       <div className='flex md:flex-row sm:flex-row justify-between items-center flex-col'>
-        <div className='w-full h-full basis-[70%] border-r border-gray-950 text-2xl uppercase m-2 p-4'>Feel Free to let us Know</div>
+        <div className='w-24 h-full basis-[50%] border-r border-gray-950 text-2xl uppercase m-2 p-4'>Feel Free to let us Know</div>
         <div className='text-xl basis-[30%] p-5 items-center'>
           <h2 className='pb-2 uppercase w-max'>Your Queries</h2>
-          <FeedBackBox/>
+          <FeedBackBox />
         </div>
       </div>
-    {/* embedded map */}
-    <div className='flex md:flex-row sm:flex-row h-[400px] justify-between items-center flex-col'>
-      <div className='w-full basis-[70%] h-full p-4'>
-        <MapComponent/>
+      {/* embedded map */}
+      <div className='flex md:flex-row sm:flex-row h-[400px] justify-between items-center flex-col'>
+        <div className='w-full basis-[70%] h-full p-4'>
+          <MapComponent />
+        </div>
+        <div className='m-2 text-2xl uppercase basis-[30%]'>
+          You can find us near Pipalbot, Dillibazzar
+        </div>
       </div>
-      <div className='m-2 text-2xl uppercase basis-[30%]'>
-        You can find us near Pipalbot, Dillibazzar
-      </div>
-    </div>
     </div>
   )
 }
 
-export default  contact
+export default contact
